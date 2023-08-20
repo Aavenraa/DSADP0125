000582baf7dec43b20f29786570dc60b520e061e
Aavenraa <139065691+Aavenraa@users.noreply.github.com>
2023-08-20 19:27:00 +0200

    Add files via upload

---
+++ Lager.js
@@ -0,0 +1,31 @@
+var Lager = {"Lager":[
+                      {"Artikelnummer":"0001", "Bezeichnung":"cookie, 100% Akryl, 200g", "Farbe":"Lila, Pink, Rot", "Preis":"23,99", "Bestand":"4"}
+                      
+                      {"Artikelnummer":"0002", "Bezeichnung":"cookie, 100% Akryl, 200g", "Farbe":"Grün, Braun, Gelb", "Preis":"23,99", "Bestand":"6"}
+                      
+                      {"Artikelnummer":"0003", "Bezeichnung":"cookie, 100% Akryl, 200g", "Farbe":"Blau, Weiß", "Preis":"23,99", "Bestand":"3"}
+                      
+                      {"Artikelnummer":"0004", "Bezeichnung":"Manyflower Cotton, 100% Baumwolle, 50g", "Farbe":"Schwarz", "Preis":"2", "Bestand":"7"}
+                      
+                      {"Artikelnummer":"0005", "Bezeichnung":"Manyflower Cotton, 100% Baumwolle, 50g", "Farbe":"Terracotta", "Preis":"2", "Bestand":"3"}
+                      
+                      {"Artikelnummer":"0006", "Bezeichnung":"Manyflower Cotton, 100% Baumwolle, 50g", "Farbe":"Bernstein", "Preis":"2", "Bestand":"1"}
+                      
+                      {"Artikelnummer":"0007", "Bezeichnung":"essentials cotton dk, 100% Baumwolle, 50g", "Farbe":"Dunkelblau", "Preis":"4", "Bestand":"10"}
+                      
+                      {"Artikelnummer":"0008", "Bezeichnung":"essentials cotton dk, 100% Baumwolle, 50g", "Farbe":"Babyblau", "Preis":"4", "Bestand":"8"}
+                      
+                      {"Artikelnummer":"0009", "Bezeichnung":"essentials cotton dk, 100% Baumwolle, 50g", "Farbe":"Dunkelgrün", "Preis":"4", "Bestand":"2"}
+                      
+                      {"Artikelnummer":"0010", "Bezeichnung":"Softcloud Strickgarn, 100% Polyester, 50g", "Farbe":"LPink", "Preis":"1", "Bestand":"15"}
+                      
+                      {"Artikelnummer":"0011", "Bezeichnung":"Softcloud Strickgarn, 100% Polyester, 50g", "Farbe":"Schwarz", "Preis":"1", "Bestand":"11"}
+                      
+                      {"Artikelnummer":"0012", "Bezeichnung":"Softcloud Strickgarn, 100% Polyester, 50g", "Farbe":"Weinrot", "Preis":"1", "Bestand":"7"}
+                      
+                      {"Artikelnummer":"0013", "Bezeichnung":"ScandinaviU Acrylic, 100% Akryl, 100g", "Farbe":"Hellblau", "Preis":"2,5", "Bestand":"5"}
+                      
+                      {"Artikelnummer":"0014", "Bezeichnung":"ScandinaviU Acrylic, 100% Akryl, 100g", "Farbe":"Weiß", "Preis":"2,5", "Bestand":"2"}
+                      
+                      {"Artikelnummer":"0015", "Bezeichnung":"ScandinaviU Acrylic, 100% Akryl, 100g", "Farbe":"Grün", "Preis":"2,5", "Bestand":"1"}
+                      ]}
